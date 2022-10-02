43c156dd425e51093e77fc783fc83c3107a2c671
Asamu victor <asamuvictor03@gmail.com>
2022-10-02 16:57:40 +0100

    created route

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
-// const cors = cors();
+const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
+app.use(cors())
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+const URI =process.env.URI
 const PORT = process.env.PORT
-
-
+mongoose.connect(URI,(err)=>{
+    if (err) {
+        console.log("error connecting with database")
+    } else {
+        console.log("connecting with database successful");
+    }
+});
 app.listen(PORT,()=>{
     console.log(`app listening at port :${PORT}`);
 })
