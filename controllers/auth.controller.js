ef8a08dc42fcd3e7058e9c1cb38fa4a7778e1314
Asamu victor <asamuvictor03@gmail.com>
2022-10-03 10:56:36 +0100

    started signin controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,33 @@
-
+const authModel = require("../models/auth.model");
 
 const signup=(req,res)=>{
     console.log(req.body);
+    authModel.findOne({email:req.body.email},(err,result)=>{
+        if(err){
+            console.log(err);
+            res.send({message:"internal server error",status:false});
+        }else{
+            if(result){
+                console.log(result,"hgfdfg")
+                res.send({message:"email has already been used",status:false});
+            }else{
+                let form = authModel(req.body);
+                form.save((err)=>{
+                    if(err){
+                     console.log(err)
+                     res.send({message:"signup not succesful",     status:false})
+                    }else{
+                     console.log("success")
+                     res.send({message:"signup successful",status:true})
+                    }
+                })
+            }
+        }
+    })
+}
+const signin=()=>{
+    console.log(req.body);
+     res.send({message:"in process though"})
 }
 
 module.exports={signup};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,4 +2,5 @@ const express = require("express");
 const auth = express.Router();
 const { signup } = require("../controllers/auth.controller");
 auth.post("/signup",signup);
+auth.post("/signin",);
 module.exports=auth;
