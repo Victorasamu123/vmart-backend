d2af9432389a494503865481d9625e24606e7cb7
Asamu victor <asamuvictor03@gmail.com>
2022-10-07 20:51:25 +0100

    addproduct controller

--- controllers/addproduct.controllers.js
+++ controllers/addproduct.controllers.js
@@ -1,2 +1,13 @@
 const phoneCatModel = require("../models/phonecat.model");
+const cloudinary = require("cloudinary")
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET,
+    secure:true
+  });
+const addphones=(req,res)=>{
+    console.log(req.body);
+    const myPhoneFile=req.body.productimage
 
+}
