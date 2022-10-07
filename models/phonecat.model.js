a0b3d03948c1d36e4c9b2b8e54f027c1a3c67b59
Asamu victor <asamuvictor03@gmail.com>
2022-10-07 20:42:53 +0100

    phone model

--- models/phonecat.model.js
+++ models/phonecat.model.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
 
-const phoneCatSchema=({
-    
-})
+const phoneCatSchema=mongoose.Schema({
+    productimage:{required:true,type:String},
+    productname:{required:true,type:String},
+    productcategory:{required:true,type:String},
+    productdescription:{required:true,type:String},
+    productprice:{required:true,type:String},
+    productdiscount:{required:true,type:String}
+});
+
+const phoneCatModel= mongoose.model("phonecat_collections",phoneCatSchema);
+
+module.exports=phoneCatModel;
